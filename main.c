dfb8d645013994867de930b4201395d13da51f98
Frozzie <yasha.petrukhin@gmail.com>
2024-04-21 13:50:46 +0300

    First task

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "matrix/matrix.h"
+#include <stdio.h>
+#include <math.h>
+
+
+int main()
+{
+    char s[10];
+    char character;
+
+    for(int i = 0; i < 10; i++)
+    {
+        scanf("%c", &character);
+        s[i] = character;
+    }
+    
+}
