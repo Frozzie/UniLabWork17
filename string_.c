ce95002af7ed0f5da30f85dde21ecd40438662c3
Frozzie <yasha.petrukhin@gmail.com>
2024-05-07 21:02:32 +0300

    Tasks 15, 16 done

--- string_.c
+++ string_.c
@@ -434,3 +434,303 @@ char *alternatingStringWords (char *s1, char *s2)
     
     return output;
 }
+
+void reverseWordOrder (char *str)
+{
+    char *buff = malloc(strlen(str) + 1), str_end;
+    WordDescriptor word;
+    str_end = buff + strlen(str);
+    
+    char *src = str;
+    char *dest = buff;
+    
+    while(*src != 0)
+    {
+        *dest++ = *src++;
+    }
+
+    while(getWordReverse(str_end, buff, &word))
+    {
+        str = copy(word.begin, word.end, str);
+    }
+
+    free(buff);
+}
+
+bool findSymInWord (char s, WordDescriptor word)
+{
+    bool flag = false;
+    while(word.begin < word.end)
+    {
+        if(word.begin == s)
+        {
+            flag = true;
+            break;
+        }
+
+        word.begin++;
+    }
+
+    return flag;
+}
+
+void printWordBeforeFirstWordWithA (char *s)
+{
+    WordDescriptor prev_word, cur_word;
+    bool flag = false, first = true;
+
+    while(getWord(s, &cur_word) != 0)
+    {
+        flag = findSymInWord('a', cur_word) || findSymInWord('A', cur_word);
+
+        if(flag)
+        {
+            if(!first)
+            {
+                printWord(prev_word);
+            }
+            break;
+        }
+
+        prev_word = cur_word;
+
+        if(first)
+        {
+            first = false;
+        }
+
+        s = cur_word.end;
+    }
+}
+
+bool wordCmp (WordDescriptor word1, WordDescriptor word2)
+{
+    bool flag;
+
+    while(word1.begin < word1.end && word2.begin < word2.end)
+    {
+        if(*word1.begin == *word2.begin)
+        {
+            word1.begin++;
+            word2.begin++;
+            flag = true;
+        }
+        else
+        {
+            flag = false;
+            break;
+        }
+    }
+
+    return flag;
+}
+
+void findWordInOtherString (char *s1, char *s2)
+{
+    WordDescriptor word1, word2, mem;
+    char s1Start = *s1, s2Start = *s2;
+    BagOfWords bag;
+    bool flag = false;
+
+    getBagOfWords(&bag, s2);
+
+    while (getWord(s1Start, &word1) == 1)
+    {
+        for (size_t i = 0; i < bag.size; i++)
+        {
+            if(wordCmp(word1, bag.words[i]))
+            {
+                mem = word1;
+                flag = true;
+            }
+        }
+    }
+
+    if(flag)
+    {
+        printWord(mem);
+    }
+}
+
+// 13
+bool FindSameWords (char *s1, char *s2)
+{
+    WordDescriptor word1, word2;
+    char s1Start = *s1, s2Start = *s2;
+    BagOfWords bag;
+    bool flag = false;
+
+    getBagOfWords(&bag, s2);
+
+    while (getWord(s1Start, &word1) == 1)
+    {
+        for (size_t i = 0; i < bag.size; i++)
+        {
+            if(wordCmp(word1, bag.words[i]))
+            {
+                flag = true;
+                break;
+            }
+        }
+    }
+
+    return flag;
+}
+
+void getAllUniqueSortedLetters (WordDescriptor *w)
+{
+    int count = 1;
+    char symMem = *w->begin;
+    char *wBegin = w->begin,  *wEnd = w->end;
+    char mem[w->end - w->begin];
+    bool flag;
+
+    mem[0] = symMem;
+
+    while(wBegin < wEnd)
+    {
+        flag = true;
+
+        for(int i = 0; i < count; i++)
+        {
+            if(wBegin == mem[i])
+            {
+                flag = false;
+                break;
+            }
+        }
+
+        if(flag)
+        {
+            mem[count++] = *wBegin++;
+        }
+    }
+
+    copy(&mem[0], &mem[count], w->begin);
+
+    w->end = w->begin + count;
+}
+
+void sortSymInWord (WordDescriptor a)
+{
+    for (char *i = a.begin; i < a.end; i++)
+    {
+        char *min_position = i;
+        char temp;
+
+        for (char *j = i + 1; j < a.end; j++)
+        {
+            if (*min_position > *j)
+            {
+                *min_position = *j;
+            }
+
+            temp = *min_position;
+            *min_position = *i;
+            *i = temp;
+        }
+    }
+}
+
+bool FindSameLettersInWord (char *s)
+{
+    WordDescriptor word, mem;
+    char sStart = *s;
+    BagOfWords bag;
+    bool flag = true;
+
+    char *s_cpy = malloc(strlen(s) + 1);
+    copy(s, s + strlen(s) + 1, s_cpy);
+    
+    getBagOfWords(&bag, s_cpy);
+
+    for(size_t i = 0; i < bag.size; i++)
+    {
+        getAllUniqueSortedLetters(&bag.words[i]);
+        sortSymInWord(bag.words[i]);
+    }
+
+    for(size_t i = 0; i < bag.size; i++)
+    {
+        for(size_t j = i + 1; j < bag.size; j++)
+        {
+            if(WordsCmp(bag.words[i], bag.words[j]) != 0)
+            {
+                flag = false;
+                break;
+            }
+        }
+    }
+
+    free(s_cpy);
+
+    return flag;
+}
+
+void getStringFromBag (BagOfWords *w, char *s)
+{
+    for(size_t i = 0; i < w->size; i++)
+    {
+        copy(w->words[i].begin, w->words[i].end, s);
+        s += w->words[i].end - w->words[i].begin;
+        *s++ = ' ';
+    }
+
+    *s = 0;
+}
+
+//17
+void deleteLastWord (char *s)
+{
+    WordDescriptor word1, word2;
+    char sStart = *s;
+    BagOfWords bag, output;
+
+    char *s_cpy = malloc(strlen(s) + 1);
+    copy(s, s + strlen(s) + 1, s_cpy);
+
+    getBagOfWords(&bag, s_cpy);
+
+    for(size_t i = 0; i < bag.size; i++)
+    {   
+        if(WordsCmp(bag.words[i], bag.words[bag.size]) != 0)
+        {
+            output.words[output.size++] = bag.words[i];
+        }
+    }
+
+    getStringFromBag(&output, s);
+
+    free(s_cpy);
+}
+
+WordDescriptor findWordBeforeFirstFoundWord (char *s1, char *s2)
+{
+    WordDescriptor word1, word2, prev_word;
+    char s1Start = *s1, s2Start = *s2;
+    BagOfWords bag1, bag2;
+    bool flag = false;
+
+    prev_word.begin = NULL;
+    prev_word.end = NULL;
+
+    getBagOfWords(&bag1, s1);
+    getBagOfWords(&bag2, s2);
+
+    for(size_t i = 0; i < bag1.size; i++)
+    {
+        for(size_t j = 0; j < bag2.size; j++)
+        {
+            if(wordCmp(bag1.words[i], bag2.words[j]) == 0)
+            {
+                if(i >= 1)
+                {
+                    prev_word = bag1.words[i - 1];
+                    break;
+                }
+            }
+        }
+    }
+
+    return prev_word;
+}
